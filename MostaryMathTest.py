501524344f257b38fbe5bf4825125aaea0a7d843
Mst.Mostary Khatun <162861911+MostaryKhatun@users.noreply.github.com>
2024-11-02 23:42:53 +0600

    Add files via upload

---
+++ MostaryMathTest.py
@@ -0,0 +1,10 @@
+# MostaryMath.py
+from mpmath import mp
+
+
+def printBigpi(n):
+    # Set precision to n decimal places
+    mp.dps = n + 1  # Adding 1 to account for the "3." before the decimals
+
+    # Return pi to n decimal places as a string
+    return str(mp.pi)
